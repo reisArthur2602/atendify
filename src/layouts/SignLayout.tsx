a81d0e9c3d3d9e129fb7e0ac073eb1c321c31e1d
dev.arthur <contato.arthurreis222@gmail.com>
2024-11-24 10:23:09 -0300

    feat: insert sign layout

--- src/layouts/SignLayout.tsx
+++ src/layouts/SignLayout.tsx
@@ -1,10 +1,21 @@
+import { Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { Logo } from "../components/Logo";
 
 export const SignLayout = () => {
   return (
-    <div>
-      SignLayout
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection:'column',
+        gap: 4,
+        height: "100vh",
+      }}
+    >
+      <Logo />
       <Outlet />
-    </div>
+    </Container>
   );
 };
