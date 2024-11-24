85ed9dcba0d40f1f66b716d31f6b5254093efd19
dev.arthur <contato.arthurreis222@gmail.com>
2024-11-24 10:36:43 -0300

    chore: update theme provider

--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-import { teal, } from '@mui/material/colors';
+import { grey, teal, } from '@mui/material/colors';
 
 export const theme = createTheme({
   palette: {
@@ -8,6 +8,38 @@ export const theme = createTheme({
     },
     secondary: {
         main: teal[800],
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '3rem',
+      fontWeight: 700,
+      color: grey[900], 
+    },
+    h2: {
+      fontSize: '2.5rem',
+      fontWeight: 600,
+      color: grey[900],
+    },
+
+    body1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.5,
+      color: grey[600],
+    },
+
+    body2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+      lineHeight: 1.43,
+      color: grey[600],
+    },
+
+    button: {
+      fontSize: '0.875rem',
+      fontWeight: 500,
+      textTransform: 'uppercase',
     },
   },
 });
